3719814dde9957be8075753948a39c466631563c
fillirus946 <fillirus946@gmail.com>
2021-01-22 15:38:15 +0300

    almost done

--- app.js
+++ app.js
@@ -1,56 +1,116 @@
 const form = document.forms.todo;
 const todo = form.elements.todoItem
 const todoList = document.querySelector('.todo__list')
-const counter = document.querySelector('.todo-numbers-title')
+const counterHtmlElement = document.querySelector('.todo-number-title')
 
-// todoList.addEventListener('click', deleteItem);
+//event listeners
+todoList.addEventListener('click', deleteItemAndCheckTodo);
+form.addEventListener('submit', formSubmit);
+//global values
+let todoCounter = 0
+const arraylistOfTodos = []
 
-form.addEventListener('submit', enter);
+//id generation
+const createRandId = () => {
+    return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(2, 10);
+}
 
-const listOfTodos = ['efwe', 'wefwef', 'wefwe']
 
-function enter(event) {
+//onformsubmit
+function formSubmit(event) {
+    const id = createRandId()
     event.preventDefault();
-
-    console.log(todo.value);
-    if(todo.value != ''){
-        listOfTodos.push(todo.value)
-    }
-
-    console.log(listOfTodos);
-    createElements(todo.value)
+    createNewTodoElement(todo.value, id)
     todo.value= ''
+
+}
+//creating new todo div and embed it in our code
+function createNewTodoElement(value, id) {
+    let newCreatedId = id
+    if(value != ''){
+        arraylistOfTodos.push({
+            text: value,
+            checked: false,
+            completed: false,
+            active: true,
+            id: newCreatedId
+        })
+        let id = newCreatedId
+        let todoText = value
+        todoList.innerHTML += `<div class="todo__task" id="${id}">
+                                                    <input type='checkbox' class="todo__task-checkbox" name="" id="">
+                                                    <div class="todo__task-info">${todoText}</div>
+                                                    <button class="todo__task-delete">X</button>
+                                                    </div>`
+        todoCounter += 1
+
+        counterHtmlElement.textContent = todoCounter
+        console.log(counterHtmlElement)
+        
+    }
 }
 
-function createElements(text) {
+// check action delete by X button or checkbox and perform
+function deleteItemAndCheckTodo(event){
 
-    const todoDiv =document.createElement("div")
-    todoDiv.classList.add('todo__task')
+    const itemOfTodoHtmlList = event.target;
+    const parent = itemOfTodoHtmlList.parentElement
+    const idOfTodoDiv = parent.getAttribute("id")
 
-    const completeCheckBox = document.createElement("input")
-    completeCheckBox.classList.add('todo__task-checkbox')
-    completeCheckBox.setAttribute('type', 'checkbox');
+    if(itemOfTodoHtmlList.classList[0] === 'todo__task-delete'){
+        parent.remove()
+        arraylistOfTodos.splice(idOfTodoDiv)
+        todoCounter -= 1
+        console.log(todoCounter)
 
-    const textOftodo = document.createElement('div')
-    textOftodo.innerText = text
-    textOftodo.classList.add('todo__task-info')
-
-    const deleteButton = document.createElement('button')
-    deleteButton.classList.add('delete-button')
-
-    todoDiv.appendChild(completeCheckBox)
-    todoDiv.appendChild(textOftodo)
-    todoDiv.appendChild(deleteButton)
-
-    console.log(todoDiv)
+    }else if(itemOfTodoHtmlList.classList[0] === 'todo__task-checkbox'){
+        for (let elem of arraylistOfTodos){
+            if(elem.id === idOfTodoDiv){
+                elem.checked = true
+                console.log(arraylistOfTodos)
+            }
+        }
+    }
 }
 
-// function deleteItem(event){
-//     const item = event.target;
-//     if(item.classList[0] === 'todo__task-delete'){
-//         console.log(item)
-//         item.remove()
-//     }
-//     console.log(target)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function createNewTodoElement(text) {
+// //create new div
+//     const todoDiv =document.createElement("div")
+//     todoDiv.classList.add('todo__task')
+// //create new checkbox in div
+//     const completeCheckBox = document.createElement("input")
+//     completeCheckBox.classList.add('todo__task-checkbox')
+//     completeCheckBox.setAttribute('type', 'checkbox');
+// //create new title of todo in div
+//     const textOftodo = document.createElement('div')
+//     textOftodo.innerText = text
+//     textOftodo.classList.add('todo__task-info')
+// //create new delete button in div
+//     const deleteButton = document.createElement('button')
+//     deleteButton.classList.add('todo__task-delete')
+//     deleteButton.innerText = 'delete'
+
+// //append all in list
+//     todoDiv.appendChild(completeCheckBox)
+//     todoDiv.appendChild(textOftodo)
+//     todoDiv.appendChild(deleteButton)
+
+//     todoList.appendChild(todoDiv)
+
 // }
-
